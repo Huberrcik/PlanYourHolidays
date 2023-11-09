a63d9f1986204ab61f3ab80f382dc26137b3de3f
Hubert Brożek <99617763+Huberrcik@users.noreply.github.com>
2023-11-09 11:02:43 +0100

    Add files via upload

---
+++ PlanYourHolidaysApp/src/main/java/com/planYourHolidays/PlanYourHolidaysApp/PlanYourHolidaysAppApplication.java
@@ -0,0 +1,13 @@
+package com.planYourHolidays.PlanYourHolidaysApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanYourHolidaysAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanYourHolidaysAppApplication.class, args);
+	}
+
+}
---
+++ PlanYourHolidaysApp/src/test/java/com/planYourHolidays/PlanYourHolidaysApp/PlanYourHolidaysAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.planYourHolidays.PlanYourHolidaysApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanYourHolidaysAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
